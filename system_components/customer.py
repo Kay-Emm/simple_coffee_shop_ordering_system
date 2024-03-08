0ebe186c3a1b96aa88f5b33697539c872db06b32
Kutlwano Modutsoane <kutlwano.modutsoane@gmail.com>
2024-03-08 20:22:29 +0200

    customer_registration, customer_data and create_database methods were defined in class: Customer. The customer class is work in  progress.

--- system_components/customer.py
+++ system_components/customer.py
@@ -1,50 +1,92 @@
 # from order import Order
+import json
+import os
+
 class Customer:
     # order = print_order()
     def __init__(self):
         # self.order = Order.print_order()
         # self.id = id
+        pass
+
+    def customer_registration_info(self) -> dict:
+
         self.name = input("Please enter your name: ").lower()
         self.surname = input("Please enter your surname: ").lower()
         self.email = input("Please enter your email address: ").lower()
         self.password1 =  input("Please enter your password: ")
         self.password2 = input("Please confirm your password: ")  
-        # self.orders =list(order)
 
-        # data = {
-        #     "id" : 1,
-        #     "name" : "kay",
-        #     "surname" : "lovell",
-        #     "email" : "kay.lovell@gmail.com",
-        #     "password"  : "TroetelDier@01"
-        # }
-    def cutsomer_data(self,  name, surname, email, password1, password2):
-        data_collection = True
+        registration_info = {"name": self.name,
+        "surname": self.surname,
+        "email": self.email,
+        "password1": self.password1,
+        "password2": self.password2
+        }
+        return registration_info
         
-        while data_collection:
-            self.name = name
-            self.surname = surname
-            self.email =email
-            self.password1 = password1
-            self.password2 = password2
-            data = []
-            if self.password1 != self.password2:
-                print("Your passwords don't match. Please")
-                self.password1
-                self.password2
+    def cutsomer_data(self) -> dict:
+        data_collection = True
+        try:
+            while data_collection:
+                registration_info = Customer.customer_registration_info(self)
+                if self.password1 != self.password2:
+                    raise PasswordMismatchError("Your passwords don't match. Please repeat the registration proces.")
+                else:
+            
+                    print("Your profile was successfully created.\n"
+                        "Please proceed to login.")
+                    print("data: ", registration_info)
+                    data_collection = False
+                return registration_info
+        except PasswordMismatchError as e:
+            print(f"Error: {e}")
+    
+    def create_database(self, data):
+    #     project_folder = os.path.dirname(os.path.abspath(__file__))
+    #     # file_path = os.path.join(project_folder, "data",  "customer_database.json")
+    #     # print("project folder: ",project_folder)
+    #     file_path = "/home/ether/Documents/github/my_repos/simple_coffee_shop_ordering_system/database/customer_database.json"
+    #     with open(file_path, "a") as json_file:
+    #         json.dump(data, json_file)
+        file_path = "/home/ether/Documents/github/my_repos/simple_coffee_shop_ordering_system/database/customer_database.json"
 
-            else:
-                # for i in range(4):
-                    data = data.append(self.name)
-                    # data.append(self.surname)
-                    # data.append(self.email)
-                    # data.append(self.password1)
-                    # data.append(self.password2)
+               # Ensure the file exists and has valid JSON content
+        if os.path.exists(file_path):
+            with open(file_path, "r") as existing_file:
+                try:
+                    existing_data = json.load(existing_file)
+                except json.JSONDecodeError:
+                    existing_data = []
+        else:
+            # If the file doesn't exist, create it with the new data
+            existing_data = []
 
-                print("Your profile was successfully created.\n" /
-                    "Please proceed to login.")
-            data_collection = False
-            pass
+        # Append the new data to the existing data
+        existing_data.append(data)
+
+        # Write the updated data back to the file
+        with open(file_path, "w") as json_file:
+            json.dump(existing_data, json_file)
+            json_file.write("\n")
+               # Ensure the file exists and has valid JSON content
+        if os.path.exists(file_path):
+            with open(file_path, "r") as existing_file:
+                try:
+                    existing_data = json.load(existing_file)
+                except json.JSONDecodeError:
+                    existing_data = []
+        else:
+            # If the file doesn't exist, create it with the new data
+            existing_data = []
+
+        # Append the new data to the existing data
+        existing_data.append(data)
+
+        # Write the updated data back to the file
+        with open(file_path, "w") as json_file:
+            json.dump(existing_data, json_file)
+            json_file.write("\n")       
 
         def sign_up(self, name, surname,email,password1, password2):
             "Welcome to Oakridge Coffee Corner! Let's have you signed up."
@@ -68,8 +110,9 @@ class Customer:
         # pass
 
     def main(self):
-
-        print(Customer.cutsomer_data(self,self.name, self.surname, self.email, self.password1, self.password2))
+    #    reg_data = Customer.customer_registration_info(self)
+       data = Customer.cutsomer_data(self)
+       Customer.create_database(self, data)
 
 if __name__ == "__main__":
 
